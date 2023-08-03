64126152d766af02a55901ff3a245be3a237e8ab
Arman Khan <Armankhan876426@gmail.com>
2023-08-03 17:49:32 +0530

    logging problem

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === String) {
   console.log = function () { };
 }
 console.log(process.env.NODE_ENV);
