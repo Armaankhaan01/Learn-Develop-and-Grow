dd3f3e3f4082727bc4f1256d7b74cedf6f71ae1c
Arman Khan <Armankhan876426@gmail.com>
2023-08-03 17:51:56 +0530

    trying again

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-if (process.env.NODE_ENV === String) {
-  console.log = function () { };
-}
-console.log(process.env.NODE_ENV);
+console.log = function () { };
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
